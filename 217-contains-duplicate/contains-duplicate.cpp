05ef2cdca55c2d7d6f6859d66f242338f2e6bcf1
this_is_easin_khandokar <113839849+mdeasinkhandokar@users.noreply.github.com>
2025-07-09 17:45:38 +0600

    Time: 103 ms (12.49%) | Memory: 95 MB (6.57%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        set<int>s;
+        for(auto u: nums){
+            s.insert(u);
+        }
+        return(s.size()!=nums.size());
+        
+    } 
+};
